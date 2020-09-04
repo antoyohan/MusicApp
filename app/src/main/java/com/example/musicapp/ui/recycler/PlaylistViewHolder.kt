19590f1835d8dba7201288a970d2bca5d7317ce1
Anto Yohan <antoyohan@gmail.com>
2020-09-04 13:31:57 -0400

    UI modified

--- app/src/main/java/com/example/musicapp/ui/recycler/PlaylistViewHolder.kt
+++ app/src/main/java/com/example/musicapp/ui/recycler/PlaylistViewHolder.kt
@@ -2,10 +2,9 @@ package com.example.musicapp.ui.recycler
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.musicapp.R
 import com.google.api.services.youtube.model.Playlist
 import com.google.api.services.youtube.model.PlaylistItem
-import com.google.api.services.youtube.model.PlaylistItemSnippet
-import com.google.api.services.youtube.model.PlaylistSnippet
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.playlist_item.view.*
 
@@ -14,11 +13,13 @@ class PlaylistViewHolder<T>(view: View): RecyclerView.ViewHolder(view) {
     fun bind(playlist: T?, cellClickListener: CellClickListener<T>?) {
         playlist?.let {
             if( playlist is Playlist) {
-                itemView.txt_news_name.text = playlist.snippet.title
+                itemView.title.text = playlist.snippet.title
+                itemView.subtitle2.text = getVideoCount(itemView, playlist.contentDetails.itemCount)
                 Picasso.get().load(playlist.snippet.thumbnails.high.url)
                     .into(itemView.img_news_banner)
             } else if (playlist is PlaylistItem) {
-                itemView.txt_news_name.text = playlist.snippet.title
+                itemView.title.text = playlist.snippet.title
+                itemView.subtitle1.text = playlist.snippet.channelTitle
                 Picasso.get().load(playlist.snippet.thumbnails.high.url)
                     .into(itemView.img_news_banner)
             }
@@ -27,5 +28,9 @@ class PlaylistViewHolder<T>(view: View): RecyclerView.ViewHolder(view) {
                 cellClickListener?.onCellClickListener(playlist)
             }
         }
+    }
+
+    private fun getVideoCount(itemView: View, itemCount: Long): CharSequence? {
+        return itemView.context.getString(if(itemCount > 1) R.string.songs_numbers else R.string.songs_number, itemCount)
     }
 }
